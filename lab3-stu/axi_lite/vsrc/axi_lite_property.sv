e9ce53e1026e13a390d2c7e2eed2e8a93f46cbb4
sunicyosen <sunicyosen@outlook.com>
2019-12-26 12:21:24 +0800

    Modified property interface

--- lab3-stu/axi_lite/vsrc/axi_lite_property.sv
+++ lab3-stu/axi_lite/vsrc/axi_lite_property.sv
@@ -1,24 +1,29 @@
 `timescale 1ns/1ps
 
 module axi_lite_property (
-    input logic    init,
-    input logic    error,txn_done,
-    input logic    clk,reset_n,
+    input logic            init,
+    input logic            error,
+    input logic            txn_done,
+    input logic            clk,
+    input logic            reset_n,
     input logic    [5:0]   awaddr,
     input logic    [2:0]   awprot,
-    input logic    awvalid,awready,
+    input logic            awvalid,
+    input logic            awready,
     input logic    [31:0]  wdata,
     input logic    [3:0]   wstrb,
-    input logic    wvalid,wready,
+    input logic            wvalid
+    input logic            wready,
     input logic    [1:0]   bresp,
-    input logic    bvalid,bready,
+    input logic            bvalid,
+    input logic            bready,
     input logic    [5:0]   araddr,
     input logic    [2:0]   arprot,
-    input logic    arvalid,arready,
+    input logic            arvalid,
+    input logic            arready,
     input logic    [31:0]  rdata,
     input logic    [1:0]   rresp,
-    input logic    rvalid,rready
-    
-);
+    input logic            rvalid,
+    input logic            rready);
 
 endmodule
